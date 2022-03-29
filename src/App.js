e4f2c7dd61038061409e7f7ac499e28e0b957c4c
benazirkhakimova <khakimovabenazir91@gmail.com>
2022-03-29 16:15:27 +0600

    finish SectionThird

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import Routing from './Routing';
-import ProductsContextProvider from './contexts/productsContext';
+import React from "react";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import Routing from "./Routing";
+import ProductsContextProvider from "./contexts/productsContext";
 
-import './App.css';
+import "./App.css";
+import SectionThird from "./components/SectionThird/SectionThird";
 
 const App = () => {
   return (
