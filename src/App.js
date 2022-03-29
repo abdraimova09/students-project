680ad6ec245b3deb91dd624a605f329204684eb5
omurkulovich <omurkulov.abs@gmail.com>
2022-03-29 20:52:51 +0600

    news absamat

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import Routing from './Routing';
-import ProductsContextProvider from './contexts/productsContext';
+import React from "react";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import Routing from "./Routing";
+import ProductsContextProvider from "./contexts/productsContext";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
   return (
