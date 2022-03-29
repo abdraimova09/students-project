98bb94714fac3c0bb58a4da469efbb840c9f56b7
makhabatabd <makhabat9898@gmail.com>
2022-03-29 15:48:50 +0600

    Makhabat's work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import Routing from './Routing';
-import ProductsContextProvider from './contexts/productsContext';
+import React from "react";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import Routing from "./Routing";
+import ProductsContextProvider from "./contexts/productsContext";
 
-import './App.css';
+import "./App.css";
+import RolexP1 from "./components/RolexP1/RolexP1";
 
 const App = () => {
   return (
     <ProductsContextProvider>
       <Routing />
+      <RolexP1 />
     </ProductsContextProvider>
   );
 };
