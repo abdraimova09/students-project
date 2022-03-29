c3996372b04e7e78f95c71948c4a8d13a48cb92b
weljeni <alsunurlanova77@gmail.com>
2022-03-29 18:00:19 +0600

    Alsu carusel

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import Routing from './Routing';
 import ProductsContextProvider from './contexts/productsContext';
 
+
+
 import './App.css';
+import WachBrand from './components/WachBrandAlsu/WachBrand';
 
 const App = () => {
   return (
     <ProductsContextProvider>
       <Routing />
+
     </ProductsContextProvider>
   );
 };
