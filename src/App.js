4cd4bc998a0308af9738d2c292ee8d9600fbb817
samarbekova-source <chikasamarbekova@gmail.com>
2022-03-29 16:18:13 +0600

    my commit watches

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import Routing from './Routing';
-import ProductsContextProvider from './contexts/productsContext';
+import React from "react";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import Routing from "./Routing";
+import ProductsContextProvider from "./contexts/productsContext";
 
-import './App.css';
+import "./App.css";
+import HomePage7BlockCardsChynara from "./components/HomePage7BlockCardsChynara/HomePage7BlockCardsChynara";
 
 const App = () => {
   return (
     <ProductsContextProvider>
       <Routing />
+      <HomePage7BlockCardsChynara />
     </ProductsContextProvider>
   );
 };
