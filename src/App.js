e23421b7bab677217f14edeb422e9ec64aefac72
musaliev <umusaliev28@gmail.com>
2022-03-30 10:07:02 +0600

    stores

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import Routing from './Routing';
-import ProductsContextProvider from './contexts/productsContext';
+import React from "react";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import Routing from "./Routing";
+import ProductsContextProvider from "./contexts/productsContext";
 
-import './App.css';
+import "./App.css";
+import Stores from "./components/StoresUlan/Stores";
 
 const App = () => {
   return (
     <ProductsContextProvider>
       <Routing />
+      <Stores />
     </ProductsContextProvider>
   );
 };
---
+++ src/components/StoresUlan/Stores.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+const Stores = () => {
+  return (
+    <div>
+      <div className="stores_1">
+        <div className="stores">
+          <img
+            src="https://content.thewosgroup.com/wosus/navigation/stores/nav-soho-nyc.jpg"
+            alt=""
+          />
+          <h4>SOHO,NYC</h4>
+        </div>
+
+        <div className="stores">
+          <img
+            src="https://content.thewosgroup.com/wosus/navigation/stores/nav-boston-ma.jpg"
+            alt=""
+          />
+          <h4>BOSTON,MA</h4>
+        </div>
+
+        <div className="stores">
+          <img
+            src="https://content.thewosgroup.com/wosus/navigation/stores/nav-wynn-lv.jpg"
+            alt=""
+          />
+          <h4>WYNN,LAS VEGAS</h4>
+        </div>
+
+        <div className="stores">
+          <img
+            src="https://content.thewosgroup.com/wosus/navigation/stores/nav-hudson-yards-nyc.jpg"
+            alt=""
+          />
+          <h4>HUDSON YARDS,NYC</h4>
+        </div>
+
+        <div className="stores">
+          <img
+            src="https://content.thewosgroup.com/wosus/navigation/stores/nav-legacy-west-tx.jpg"
+            alt=""
+          />
+          <h4>LEGACY WEST,TX</h4>
+        </div>
+
+        <div className="stores">
+          <img
+            src="https://content.thewosgroup.com/wosus/navigation/stores/nav-moa-mn.jpg"
+            alt=""
+          />{" "}
+          <h4>MALL OF AMERICA,MN</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Stores;
