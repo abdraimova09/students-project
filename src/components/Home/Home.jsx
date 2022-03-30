898da69756e6f941f1b74db809898e485f8ceb11
Begimay Abdraimova <abdraimova09@gmail.com>
2022-03-30 10:29:07 +0600

    merge with eldiyar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import HomePageBekbolsun from '../HomePageBekbolsun/HomePageBekbolsun';
 import HomePageWatchesBlock from '../HomePageWatchesBlock/HomePageWatchesBlock'
 import SectionThird from '../SectionThird/SectionThird'
+import CarouselEldiyar from '../CarouselEldiyar/CarouselEldiyar'
 const Home = () => {
     return (
         <div>
             <HomePageBekbolsun />
             <HomePageWatchesBlock />
             <SectionThird />
+            <CarouselEldiyar />
         </div>
     );
 };
